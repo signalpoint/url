64afb57e59b1133cb02e1219ad76f0f906219fff
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-02-12 18:23:30 -0500

    Add theme_url().

--- url.js
+++ url.js
@@ -6,15 +6,28 @@ function url_field_formatter_view(entity_type, entity, field, instance, langcode
     //dpm(items);
     var element = {};
     $.each(items, function(delta, item) {
-        var title = item.title ? item.title : item.value
         element[delta] = {
-          markup: theme(
-            'button_link',
-            { text: title, path: item.value, options: { InAppBrowser: true } }
-          )
+          markup: theme('url', { item: item })
         };
     });
     return element;
   }
   catch (error) { console.log('url_field_formatter_view - ' + error); }
+}
+
+/**
+ * Themes a URL.
+ */
+function theme_url(variables) {
+  try {
+    var title = variables.item.title ? variables.item.title : variables.item.value;
+    return theme('button_link', {
+        text: title,
+        path: variables.item.value,
+        options: {
+          InAppBrowser: true
+        }
+    });
+  }
+  catch (error) { console.log('theme_url - ' + error); }
 }
